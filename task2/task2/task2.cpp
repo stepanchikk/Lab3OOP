236760e830a2a5e99ce87c8e112e8f0d0dda6d1f
stepanchikk <122825623+stepanchikk@users.noreply.github.com>
2025-03-28 00:10:57 +0200

    пояснення task2

--- task2/task2/task2.cpp
+++ task2/task2/task2.cpp
@@ -8,12 +8,12 @@ private:
 
 public:
     Vector() { objectCount++; } // Конструктор без параметрів
-    Vector(double value) { data[0] = data[1] = data[2] = value; objectCount++; }
-    Vector(double x, double y, double z) { data[0] = x; data[1] = y; data[2] = z; objectCount++; }
-    Vector(const double* arr) { if (arr) for (int i = 0; i < 3; i++) data[i] = arr[i]; objectCount++; }
-    ~Vector() { objectCount--; }
+    Vector(double value) { data[0] = data[1] = data[2] = value; objectCount++; } //Конструктор з одним параметром
+    Vector(double x, double y, double z) { data[0] = x; data[1] = y; data[2] = z; objectCount++; } //
+    Vector(const double* arr) { if (arr) for (int i = 0; i < 3; i++) data[i] = arr[i]; objectCount++; } //Конструктор з масивом
+    ~Vector() { objectCount--; } //Деструктор зменшує лічильник об'єктів objectCount, коли об'єкт знищується
 
-    void print() const { std::cout << "[" << data[0] << ", " << data[1] << ", " << data[2] << "]\n"; }
+    void print() const { std::cout << "[" << data[0] << ", " << data[1] << ", " << data[2] << "]\n"; } //Функція друку: виводить координати вектора на екран
     Vector add(const Vector& v) const { return Vector(data[0] + v.data[0], data[1] + v.data[1], data[2] + v.data[2]); }
     Vector subtract(const Vector& v) const { return Vector(data[0] - v.data[0], data[1] - v.data[1], data[2] - v.data[2]); }
     Vector crossProduct(const Vector& v) const {
@@ -23,8 +23,8 @@ public:
             data[0] * v.data[1] - data[1] * v.data[0]
         );
     }
-    Vector divide(short divisor) const { return divisor == 0 ? *this : Vector(data[0] / divisor, data[1] / divisor, data[2] / divisor); }
-    static int getObjectCount() { return objectCount; }
+    Vector divide(short divisor) const { return divisor == 0 ? *this : Vector(data[0] / divisor, data[1] / divisor, data[2] / divisor); } //функція divide виконує ділення елементів вектора на ціле число divisor
+    static int getObjectCount() { return objectCount; } //Лічильник об'єктів: функція getObjectCount повертає кількість створених об'єктів
 };
 
 int Vector::objectCount = 0;
@@ -32,7 +32,7 @@ int Vector::objectCount = 0;
 int main() {
     Vector v1, v2(5.5), v3(1, 2, 3);
 
-    double arr[] = { 4, 5, 6 };
+    double arr[] = { 4, 5, 6 }; //Оголошення масиву та ініціалізація вектора
     Vector v4(arr); // Правильна передача масиву
 
     Vector sum = v2.add(v3), diff = v2.subtract(v3), cross = v2.crossProduct(v3), divided = v3.divide(2);
